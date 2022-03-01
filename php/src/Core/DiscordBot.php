2ab06ef99341d6955a012d1f37520db3494e997f
Manuel-Antunes <nerdbroz2@gmail.com>
2022-03-01 12:28:16 -0300

    feat: php bot

---
+++ php/src/Core/DiscordBot.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TavernaDeMordor\Bot\Core;
+
+use Discord\Discord;
+
+class DiscordBot
+{
+  private Discord $client;
+  private String $token;
+
+  public function __construct(String $token)
+  {
+    $this->token = $token;
+    $this->client = new Discord(['token' => $token]);
+  }
+
+  public function start()
+  {
+    print("Rodando");
+    $this->client->run();
+  }
+}
--- php/src/main.php
+++ php/src/main.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "hello world";
+use TavernaDeMordor\Bot\Core\DiscordBot;
+
+function main()
+{
+  $bot = new DiscordBot("OTQ4MjM0NjQzNjQ2MzE2NjU0.Yh42VA.ik2kYLqooOi_ToQXSMHfUbuIHmQ");
+  $bot->start();
+}
+
+main();
