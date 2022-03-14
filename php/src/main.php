22cabadb82983a85ae1700945a2682c052e81661
victorlucasrobs <88686013+victorlucasrobs@users.noreply.github.com>
2022-03-14 00:36:36 -0300

    Update php/src/main.php
    Co-authored-by: Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>

--- php/src/main.php
+++ php/src/main.php
@@ -9,7 +9,7 @@ function main()
 {
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
-    $client = new Discord([$_ENV["DISCORD_TOKEN"]]);
+    $client = new Discord([ 'token' => $_ENV["DISCORD_TOKEN"] ]);
     $bot = new DiscordBot($client, "/");
     $bot->start();
 }
